4918d86f1fc48597106b65b37b3e365ce62b15f3
liat g <liatg812@gmail.com>
2024-02-20 13:07:23 -0500

    amplify site

---
+++ Desktop/working_portfolio/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    // <main>
+        <div>
+    <h1>
+    Liat Arkadia Guvenc
+    </h1>
+    <subtitle>
+        fullstack web developer from NY
+    </subtitle>
+    </div>
+    /* </main> */
+  )
+}
+
+export default Navbar
