4753b83b95d44213ff5fddb5da9359c7b7e01107
liat g <liatg812@gmail.com>
2024-02-20 23:15:54 -0500

    changed file directory

---
+++ working_portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <box>
+      <Router>
+        <Navbar />
+        <Projects />
+        <Contact />
+
+      </Router>
+        </box>
+    </>
+  )
+}
+
+export default App
